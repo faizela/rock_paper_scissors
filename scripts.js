962ca94c71b9702a553251cd02af5350debea4f5
Feisal Ahmed <feisala132@gmail.com>
2024-01-28 23:25:03 +0000

    Adding getComputerChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log('Hello world')
+function getComputerChoice () {
+    const choices = ['rock', 'paper', 'scissors']
+    const random_choice = Math.round(Math.random() * choices.length)
+    return choices[random_choice]
+}
+
+console.log(getComputerChoice())
