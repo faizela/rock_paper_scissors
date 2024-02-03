0da16175e9f093d0210100515e0305adac13d1a3
Feisal Ahmed <feisala132@gmail.com>
2024-02-03 14:23:10 +0000

    changing name of singleRound Function to playRound function

--- scripts.js
+++ scripts.js
@@ -6,23 +6,102 @@ function getComputerChoice () {
 
 
 
-
-const playerSelection = 'rOCK'
+const playerSelection = prompt('Enter your choice - rock or paper or scissors! ')
 const computerSelection = getComputerChoice()
 
+let cpuScore = 0
+let playerScore = 0
 
-function singleRound(playerSelection, computerSelection) {
- if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors'){
-   return ('You WIN! rock beats scissors')
- } 
- else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
-    return ('You LOSE! paper beats rock')
- }
- else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
-    return singleRound(playerSelection, computerSelection = getComputerChoice())
- }
+
+function isRock(playerSelection, computerSelection) {
+   if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors'){
+      playerScore += 1
+      return ('You WIN! rock beats scissors')
+    } 
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+      cpuScore += 1
+       return ('You LOSE! paper beats rock')
+    }
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
+       return playRound(playerSelection, getComputerChoice())
+    }
+      
 }
 
 
-console.log(singleRound(playerSelection, computerSelection))
+
+function isScissors(playerSelection, computerSelection) {
+   if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
+      playerScore += 1
+       return ('You WIN! scissors beats rock ')
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
+      cpuScore += 1
+       return ('You LOSE! rock beats scissors')
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors'){
+       return playRound(playerSelection, getComputerChoice())
+    }
+}
+
+function isPaper(playerSelection, computerSelection){
+   if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+      playerScore += 1
+      return ('You WIN! paper beats rock ')
+   }
+   else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors'){
+      cpuScore += 1
+      return ('You LOSE! scissors beats paper')
+   }
+   else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper'){
+      return playRound(playerSelection, getComputerChoice())
+   }
+  
+}
+
+function playRound(playerSelection, computerSelection) {
+ if (playerSelection.toLowerCase() == 'rock') {
+   return isRock(playerSelection, computerSelection)
+ }
+
+ else if  (playerSelection.toLowerCase() == 'scissors') {
+   return isScissors(playerSelection, computerSelection)
+ }
+
+ else if (playerSelection.toLowerCase() == 'paper') {
+   return isPaper(playerSelection, computerSelection)
+ }
+
+
+}
+
+// console.log(cpuScore)
+// console.log(playerScore)
+// console.log(playRound(playerSelection, computerSelection))
+// console.log(cpuScore)
+// console.log(playerScore)
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+      console.log(`round ${i+1}`)
+      console.log(playRound(playerSelection, computerSelection))
+      console.log(`Player score is ${playerScore}`)
+      console.log(`Cpu score is ${cpuScore}`)
+    }
+
+   if (cpuScore > playerScore){
+      console.log('CPU wins!')
+   }
+   else if (playerScore > cpuScore) {
+      console.log('Player Wins!')
+   }
+}
+
+game()
+
+// console.log(playRound(playerSelection, computerSelection))
+// console.log(playRound(playerSelection, computerSelection))
+// console.log(playRound(playerSelection, computerSelection))
+// console.log(playRound(playerSelection, computerSelection))
+// console.log(playRound(playerSelection, computerSelection))
 
